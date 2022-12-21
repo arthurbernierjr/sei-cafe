ace02d2f57ce106e4056531c4767df9e05ac79ee
Arthur Bernier <ma2bernier@gmail.com>
2022-12-21 18:03:13 -0500

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const usersCtrl = require('../../controllers/api/users');
+// /routes/api/users.js
+const express = require('express')
+const router = express.Router()
+const { checkToken, dataController, apiController } = require('../../controllers/api/users')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 // POST /api/users
-router.post('/', usersCtrl.create);
+router.post('/', dataController.create, apiController.auth)
 // POST /api/users/login
-router.post('/login', usersCtrl.login);
+router.post('/login', dataController.login, apiController.auth)
 
-module.exports = router;
+// GET /api/users/check-token
+router.get('/check-token', ensureLoggedIn, checkToken)
+
+module.exports = router
 
